e11c8253c5dfc2c96e9f82aea170e3346adf4c97
Balakrishnan R <19cad216@gmail.com>
2025-06-18 20:41:41 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function getDB(){
-    mongoose.connect('mongodb+srv://bala123:chinnuchinni@cluster0.b0m3w.mongodb.net/restuarant?retryWrites=true&w=majority')
+    mongoose.connect('mongodb+srv://bala123:bala1234@cluster0.b0m3w.mongodb.net/restuarant?retryWrites=true&w=majority')
     .then(result => console.log('DB Connected!'))
     .catch(e => console.log(e));
 }
